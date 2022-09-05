f1d2283ab90ef362117c8331dc614795e2b7ab44
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2022-09-06 04:36:55 +0800

    Implement Uploading New Recipe, add JS docs and some refactors

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'bad6a1bf-b04b-4c16-b363-bddcef0c8a38';
+export const MODAL_CLOSE_SEC = 2.5;
